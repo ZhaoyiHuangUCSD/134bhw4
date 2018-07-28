3da8fd281e900874937a58d4c617e8de366963b4
ZhaoyiHuangUCSD <32404758+ZhaoyiHuangUCSD@users.noreply.github.com>
2018-07-28 00:24:19 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,60 @@
+/* This is the js used for index, link to firebase*/
+
+function login(){
+
+var email= document.getElementById("email").value;
+var password=document.getElementById("password").value;
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
+}    
+
+    firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    
+    window.location="./issue-table.html";
+  }
+});
+ 
+
+/*preserve the style when offline*/
+ workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+); 
+
+ workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  workbox.strategies.staleWhileRevalidate(),
+); 
+
+ workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+); 
