b5f3ca4c27c94f8cdd50d7d10d82e1e7da8a496c
ZhaoyiHuangUCSD <32404758+ZhaoyiHuangUCSD@users.noreply.github.com>
2018-07-31 16:45:52 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,59 +1,43 @@
-/* This is the js used for index, link to firebase */
+/* This is the js used for index, link to firebase*/
 
-function login() {
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode === 'auth/wrong-password') {
-      alert('Wrong password.');
-    } else {
-      alert(errorMessage);
-    }
-    console.log(error);
-    document.getElementById('quickstart-sign-in').disabled = false;
-    // [END_EXCLUDE]
-  });
+function login(){
+
+var email= document.getElementById("email").value;
+var password=document.getElementById("password").value;
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
 }    
 
     firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     
-    window.location='./issue-table.html';
+    window.location="./issue-table.html";
   }
-})
- 
+});
 
-/*preserve the style when offline*/
- workbox.routing.registerRoute(
-  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-  workbox.strategies.cacheFirst({
-    cacheName: 'google-fonts',
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200],
-      }),
-    ],
-  }),
-)
 
- workbox.routing.registerRoute(
-  /\.(?:js|css)$/,
-  workbox.strategies.staleWhileRevalidate(),
-); 
 
- workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'images',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      }),
-    ],
-  }),
-); 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
